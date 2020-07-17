8136b35d7ebd1bbdebe1212bd70f8279ce303b68
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2020-07-16 22:40:34 -0700

    Card.cpp/.h updates
    - implemented and defined non-member operator<< for class Card
    - updated Card class to use QString instead of char for field "suit" and updated functions accordingly
    - cleaned up comments

--- Card.cpp
+++ Card.cpp
@@ -5,7 +5,32 @@ unsigned short Card::getRank() const
     return rank;
 }
 
-char Card::getSuit() const
+QString Card::getSuit() const
 {
     return suit;
+}
+
+// non-member card output
+std::ostream& operator<<(std::ostream& out, Card c)
+{
+    switch (c.getRank())
+    {
+    case 1:
+        out << "ace";
+        break;
+    case 11:
+        out << "jack";
+        break;
+    case 12:
+        out << "queen";
+        break;
+    case 13:
+        out << "king";
+        break;
+    default:
+        out << c.getRank();
+        break;
+    }
+    out << " of " << c.getSuit().toStdString();
+    return out;
 }
--- Card.h
+++ Card.h
@@ -1,22 +1,27 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include "qchar.h"
+#include "qstring.h"
+#include <iostream>
+
 class Card
 {
+    friend std::ostream& operator<<(std::ostream&, Card);
 private:
-    unsigned short rank;
-    char suit;
+    unsigned short rank; // 1-13
+    QString suit; // C D H S
 public:
 
     // initializer list ctor
-    Card(unsigned short r, char s): rank(r), suit(s) {}
+    Card(unsigned short r, QString s): rank(r), suit(s) {}
 
     // run time dtor
     ~Card() = default;
 
     // getter funcs
     unsigned short getRank() const;
-    char getSuit() const;
+    QString getSuit() const;
 };
 
-#endif
+#endif // CARD_H
